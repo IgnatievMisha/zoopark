533fac755dc4ebaa5573fc2be43870e6e6a292d7
IgnatievMisha <mishaross1604@gmail.com>
2023-05-18 18:16:43 +0300

    AAAAAAAAAAAAAAaaa?

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import random
 class Animal:
-    def __init__(self, species, name, age, health, hunger, happiness):
+    def __init__(self, species, name, age, health, hunger, happines):
         super().__init__()
-        self.apecies=species
+        self.species=species
         self.name=name
         self.age=age
         self.health=health
@@ -21,9 +21,32 @@ class Animal:
         else:
             print(self.name, "hungry")
     def play(self):
-        pass #happines
+        if self.happines>=5:
+            self.health+=random.randint(0, 5)
+            self.hunger += random.randint(0, 5)
+            self.happines += random.randint(0, 5)
+        else:
+            print(self.name, "does not want to play")
+    def __str__(self):
+        return f"{self.species} - {self.name} ({self.age} years old)\n" \
+        f"Health: {self.health}\nHunger: {self.hunger}\nHappines: {self.happines}\n"
 
 class Zoo:
     def __init__(self):
         super.__init__()
-        self.animal=[]
+        self.animals=[]
+
+    def add_animal(self, animal):
+        self.animals.append(animal)
+    def remove_animal(self, animal):
+        if animal in self.animals:
+            self.animals.remove(animal)
+    def feed_all(self):
+        if animal in self.animals:
+            animal.eat()
+    def ply_with(self, animal):
+        if animal in self.animals:
+            animal.play()
+    def grow_all(self):
+        if animal in self.animals:
+            animal.grow()
